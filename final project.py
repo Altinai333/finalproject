9db191df52196a98dcbb88ba94f4a0acc0847c0a
Altinai333 <atoucheteau@putnamscience.org>
2024-11-14 13:03:35 -0500

    Update final project.py

--- final project.py
+++ final project.py
@@ -1,1 +1,7 @@
-print("hello world")
+print("enter a book title")
+list = ["The animal farm", "The handmaid's tale", "The catcher in the rye"]
+price = float(0)
+bookstore = input("enter a book title")
+if bookstore 
+
+
