ad518d659c156c0d90e138c76b2fb9558374dddd
Altinai333 <atoucheteau@putnamscience.org>
2024-11-18 14:19:35 -0500

    did def

--- final project.py
+++ final project.py
@@ -1,11 +1,29 @@
+from storage import Library 
+
+
 while True:
-  print("welcome to the bookstore")
-  print("please choose a book")
-  person = input("A. choose a book \n B. Add books \n C. Sell books")
+  print("\n Welcome to the bookstore")
 
-  list = ["The animal farm", "The handmaid's tale", "The catcher in the rye", "The great gatsby"]
+  person = input("\n A. See our catalog of books \n B. Add books \n C. Sell books \n D.Quit")
 
- if person == "A"
-   print list 
+  list = "Animal Farm by George Orwell", "The Handmaid's Tale by Margaret Atwood", "The Catcher in the Rye by J.D Salinger", "The Great Gatsby by Fitzgerald", "Things Fall Apart by Chinua Achebe"
+
+  if person == "A":
+    print(list)
+
+  if person == "B":
+    Library.add()
+
+  if person == "C":
+    Library.sell()
+
+  if person == "Q":
+    quit()
+  
+
+
+
+
+
 
 
---
+++ storage.py
@@ -0,0 +1,26 @@
+class Library():
+    def add():
+
+      print("Add the book of your choice\n")
+      moretitle = input("Title:")
+      moreauthor = input("Author:")
+      print(moretitle)
+      print(moreauthor)
+
+
+    def sell():
+       market = input ("Sell the book of your choice\n")
+       price = input ("Put your price:")
+       print(market)
+       print("The price is now set at", price )
+       
+
+    
+       
+     
+    
+    
+""" 
+    
+     
+    def books():  """
