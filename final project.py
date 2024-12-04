ed9bcaefa8bcae2fed77def5ba1f9ad9fb6348de
Altinai333 <atoucheteau@putnamscience.org>
2024-12-04 14:22:08 -0500

    created loop

--- final project.py
+++ final project.py
@@ -5,10 +5,10 @@ data = Library()
 while True:
   print("\n Welcome to the bookstore !")
 
-  person = input("\n A. See our catalog of books \n B. Add books \n C. Sell books \n D. See the price of our books \n E.Quit \n Enter the letter that corresponds to your choice: " " ")
+  person = input("\n A. See our catalog of books \n B. Add books \n C. Sell books \n D. See the price of our books \n E. Quit \n Enter the letter that corresponds to your choice: " " ")
 
   if person == "A":
-    print("Here are the books we have:\n") 
+    print("\n Here are the books we have:\n") 
     data.list()
 
   elif person == "B":
--- storage.py
+++ storage.py
@@ -4,25 +4,24 @@ class Library():
        self.moreprice = float(0)
        self.moretitle = ""
        self.moreauthor = ""
-
        
-
     def add(self):
        print("Add the book of your choice \n")
        self.moretitle = input("Title:")
        self.moreauthor = input("Author:")
        self.moreprice = input("Price:")
-       self.books[self.moretitle] = {self.moreauthor, self.moreprice}
+       self.books[self.moretitle] = {"writer":self.moreauthor, "price": self.moreprice}
        print(self.books)
-       print(self.moretitle, "by", self.moreauthor, "is now in the bookstore")
+       print(self.moretitle, "by", self.moreauthor, "is now added to the bookstore")
 
        
     def sell(self):
        self.moretitle = input ("Sell the book of your choice\n")
        price = input ("Put your price:")
        print(self.moretitle)
-       print("The price is now set at", price, "dollars" )
-       my_sell.pop 
+       print("You sold your book for", price, "dollars" )
+       self.books.pop(self.moretitle)
+       print(self.books)
 
     def show(self):
        cost = input ("See our prices")
@@ -31,14 +30,11 @@ class Library():
        print(cost1)
 
     def list(self):
-       self.books = input("\n The Handmaid's Tale by M.Atwood \n Animal Farm by G.Orwell \n The Catcher in the Rye by J.D Salinger \n The Great Gatsby by Fitzgerald \n Things Fall Apart by C.Achebe")
-       print(self.books)
-       my_list = {"\n The Handmaid's Tale by M.Atwood \n Animal Farm by G.Orwell \n The Catcher in the Rye by J.D Salinger \n The Great Gatsby by Fitzgerald \n Things Fall Apart by C.Achebe"}
-       my_list.update({'Romeo and Juliet by Shakespeare'})
-       print(my_list)
+       for a in self.books:
+          print(a, self.books[a]["writer"])
+          print(["price"])
 
 
-"have the option to save changes" 
 
 
     
